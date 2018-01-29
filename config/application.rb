b3f305486ed1f837108369859e5c6bce38f7e2e6
tomBeach <tbeach2k@gmail.com>
2018-01-29 11:05:18 -0500

    uppercase module name

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module forestsat_demo
+module Forestsat_demo
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
